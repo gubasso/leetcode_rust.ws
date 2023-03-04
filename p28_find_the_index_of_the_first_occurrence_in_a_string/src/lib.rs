7328e46ee6736a41b8b3c3c436ee124562ea0089
Gustavo Basso <gubasso@cwnt.io>
2023-03-03 22:38:12 -0300

    (locked) Issue: p28_find_the_index_of_the_first_occurrence_in_a_string make it work (#1)

--- p28_find_the_index_of_the_first_occurrence_in_a_string/src/lib.rs
+++ p28_find_the_index_of_the_first_occurrence_in_a_string/src/lib.rs
@@ -1,6 +1,22 @@
 pub fn str_str(haystack: String, needle: String) -> i32 {
-
-    0
+    let first_letter: char = needle.chars().next().unwrap();
+    for (hi, hc) in haystack.chars().enumerate() {
+        if hc == first_letter {
+            let mut is_equal: bool = false;
+            let mut loop_counter: usize = hi;
+            for nc in needle.chars() {
+                is_equal = if let Some(hc_char) = haystack.chars().nth(loop_counter) {
+                    nc == hc_char
+                } else { false };
+                if !is_equal { break; };
+                loop_counter += 1;
+            }
+            if is_equal {
+                return hi as i32;
+            };
+        }
+    }
+    -1
 }
 
 #[cfg(test)]
@@ -10,6 +26,13 @@ mod tests {
     #[test]
     fn case_1() {
         let result = str_str("sadbutsad".to_string(), "sad".to_string());
-        assert_eq!(result, 4);
+        assert_eq!(result, 0);
     }
+
+    #[test]
+    fn case_2() {
+        let result = str_str("leetcode".to_string(), "leeto".to_string());
+        assert_eq!(result, -1);
+    }
+
 }
