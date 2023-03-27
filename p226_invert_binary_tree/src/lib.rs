93d402e9228187976ac1bca1a8ca515d3bbee7b8
Gustavo Basso <gubasso@eambar.net>
2023-03-27 15:38:15 -0300

    (locked) Issue: p226_invert_binary_tree pass all leetcode tests (#1)

--- p226_invert_binary_tree/src/lib.rs
+++ p226_invert_binary_tree/src/lib.rs
@@ -21,8 +21,9 @@ impl TreeNode {
 }
 
 fn convert_vec_to_tree_node(vec: &Vec<i32>, i: usize) -> Option<Rc<RefCell<TreeNode>>> {
-    let mut node: TreeNode = TreeNode::new(vec[0]);
     let n: usize = vec.len();
+    if n == 0 { return None; };
+    let mut node: TreeNode = TreeNode::new(vec[0]);
 
     if i < n {
         node.left = convert_vec_to_tree_node(vec, 2*i + 1);
@@ -76,6 +77,27 @@ mod tests {
         println!("\n");
         let vec_input = vec![4,2,7,1,3,6,9];
         let vec_answer = vec![4,7,2,9,6,3,1];
+        let tree_input = convert_vec_to_tree_node(&vec_input, 0);
+        let inv_tree_input = invert_tree(&tree_input);
+        let inv_vec = convert_tree_node_to_vec(&inv_tree_input);
+        assert_eq!(inv_vec, vec_answer);
+    }
+
+    #[test]
+    fn t2() {
+        println!("\n");
+        let vec_input = vec![2,1,3];
+        let vec_answer = vec![2,3,1];
+        let tree_input = convert_vec_to_tree_node(&vec_input, 0);
+        let inv_tree_input = invert_tree(&tree_input);
+        let inv_vec = convert_tree_node_to_vec(&inv_tree_input);
+        assert_eq!(inv_vec, vec_answer);
+    }
+
+    #[test]
+    fn t3() {
+        let vec_input = vec![];
+        let vec_answer = vec![];
         let tree_input = convert_vec_to_tree_node(&vec_input, 0);
         let inv_tree_input = invert_tree(&tree_input);
         let inv_vec = convert_tree_node_to_vec(&inv_tree_input);
