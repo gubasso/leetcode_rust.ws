d41667842f4dd02eda8c164392be26106c0599d3
Gustavo Basso <gubasso@eambar.net>
2023-04-05 20:41:51 -0300

    (locked) Issue:  (#f)

--- p412_fizz_buzz/src/lib.rs
+++ p412_fizz_buzz/src/lib.rs
@@ -3,12 +3,11 @@ struct Solution;
 impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
         let mut v = Vec::new();
-        for i in 0..n {
-            let pos = i + 1;
+        for pos in 1..=n {
             let mut string: String = "".into();
             if pos % 3 == 0 { string.push_str("Fizz") };
             if pos % 5 == 0 { string.push_str("Buzz") };
-            if !(pos % 3 == 0 || pos % 5 == 0) { string.push_str(&pos.to_string()) };
+            if string.is_empty() { string.push_str(&pos.to_string()) };
             v.push(string);
         }
         v
