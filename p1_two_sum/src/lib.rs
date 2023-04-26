0b6fc16910e348256f9e140f5a7d9da3c96aa791
Gustavo Basso <gubasso@eambar.net>
2023-04-26 19:12:33 -0300

    (locked) Issue: p1_two_sum (#1)

---
+++ p1_two_sum/src/lib.rs
@@ -0,0 +1,46 @@
+struct Solution;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut coord: Vec<i32> = Vec::new();
+        let mut left: usize = 0;
+        let mut right: usize = 1;
+
+        loop {
+            if nums[left] + nums[right] == target {
+                coord = vec![left as i32, right as i32];
+                return coord;
+            }
+            right += 1;
+            if right == nums.len() {
+                left += 1;
+                right = left + 1;
+                if right == nums.len() { break; }
+            }
+        }
+        coord
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn t1() {
+        let result = Solution::two_sum(vec![2,7,11,15], 9);
+        assert_eq!(result, vec![0,1]);
+    }
+
+    #[test]
+    fn t2() {
+        let result = Solution::two_sum(vec![3,2,4], 6);
+        assert_eq!(result, vec![1,2]);
+    }
+
+    #[test]
+    fn t3() {
+        let result = Solution::two_sum(vec![3,3], 6);
+        assert_eq!(result, vec![0,1]);
+    }
+
+}
