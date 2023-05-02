0357a578f12b1d77a6e985da710d42c2edaecee6
Gustavo Basso <gubasso@eambar.net>
2023-05-02 20:57:30 -0300

    (locked) Issue: icc_minimum_value_to_get_positive_step_by_step_sum (#1)

--- icc_minimum_value_to_get_positive_step_by_step_sum/src/lib.rs
+++ icc_minimum_value_to_get_positive_step_by_step_sum/src/lib.rs
@@ -1,5 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+struct Solution;
+impl Solution {
+    pub fn min_start_value(nums: Vec<i32>) -> i32 {
+        let mut last_sum = 0;
+        let mut min: i32 = 0;
+
+        for i in 0..nums.len() {
+            last_sum += nums[i];
+            min = i32::min(min, last_sum);
+        }
+
+        - min + 1
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +18,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
+    fn t1() {
+        let result = Solution::min_start_value(vec![-3,2,-3,4,2]);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn t2() {
+        let result = Solution::min_start_value(vec![1,2]);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn t3() {
+        let result = Solution::min_start_value(vec![1,-2,-3]);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn t4() {
+        let result = Solution::min_start_value(vec![-3,6,2,5,8,6]);
         assert_eq!(result, 4);
     }
+
 }
