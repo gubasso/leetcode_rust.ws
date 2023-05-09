621e1273651d5485d6f0c418dd0b591ee59d71ae
Gustavo Basso <gubasso@eambar.net>
2023-05-09 17:20:17 -0300

    (locked) Issue: p560_subarray_sum_equals_k (#1)

--- p560_subarray_sum_equals_k/src/lib.rs
+++ p560_subarray_sum_equals_k/src/lib.rs
@@ -3,25 +3,18 @@ use std::collections::HashMap;
 struct Solution;
 impl Solution {
     pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
-        let mut ans = 0;
-        let mut sum = 0;
+        let (mut sum, mut ans) = (0, 0);
         let mut map: HashMap<i32, i32> = HashMap::new();
-        map.insert(0, 0);
+        map.insert(0, 1);
 
         for n in nums {
             sum += n;
-            let wished_sum = sum - k;
-            let map_wished_sum_val = map.get(&wished_sum).cloned();
-
-            if let Some(_) = map_wished_sum_val {
-                let mut map_to_add = *map.get_mut(&wished_sum).unwrap();
-                map_to_add += 1;
-                ans += map_to_add;
-                map.entry(sum).and_modify(|val| *val += 1).or_insert(0);
-            } else {
-                map.entry(sum).and_modify(|val| *val += 1).or_insert(0);
+            if let Some(&val) = map.get(&(sum-k)) {
+                ans += val;
             }
+            *map.entry(sum).or_insert(0) += 1;
         }
+
         ans
     }
 }
