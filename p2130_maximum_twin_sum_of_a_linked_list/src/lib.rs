533ed9e8d08470bda5befa7716d53e53a917df9b
Gustavo Basso <gubasso@eambar.net>
2023-06-14 21:33:58 -0300

    (locked) Issue: p2130_maximum_twin_sum_of_a_linked_list (#1)

--- p2130_maximum_twin_sum_of_a_linked_list/src/lib.rs
+++ p2130_maximum_twin_sum_of_a_linked_list/src/lib.rs
@@ -19,8 +19,8 @@ fn vec_to_list(vec: &Vec<i32>) -> Option<Box<ListNode>> {
     if vec.is_empty() { return None; }
     let mut list = Some(Box::new(ListNode::new(vec[0])));
     let mut pointer = &mut list;
-    for i in 1..vec.len() {
-        pointer.as_mut().unwrap().next = Some(Box::new(ListNode::new(vec[i])));
+    for e in vec.iter().skip(1) {
+        pointer.as_mut().unwrap().next = Some(Box::new(ListNode::new(*e)));
         pointer = &mut pointer.as_mut().unwrap().next;
     };
     list
@@ -30,46 +30,36 @@ struct Solution;
 impl Solution {
     pub fn pair_sum(head: Option<Box<ListNode>>) -> i32 {
         let mut sum = 0;
-        let mut l1 = Some(Box::new(ListNode::new(0)));
-        l1.as_mut().unwrap().next = head;
 
         // accept the solution that clones list 2
-
-        let mut slow = &l1.as_ref().unwrap().next;
-        let mut fast = &l1.as_ref().unwrap().next;
+        let mut slow = &head;
+        let mut fast = &head;
 
         while fast.as_ref().is_some() && fast.as_ref().unwrap().next.is_some() {
             slow = &slow.as_ref().unwrap().next;
             fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
         }
 
-        let mut l2 = Some(Box::new(ListNode::new(0)));
-        l2.as_mut().unwrap().next = slow.clone();
-        let mut curr = &mut l2.as_mut().unwrap().next;
+        let mut curr = slow.clone();
         let mut prev: Option<Box<ListNode>> = None;
 
-        // invert l2
-        while let Some(node) = curr {
-
+        // reverse / invert l2
+        while let Some(mut node) = curr {
+            curr = node.next;
+            node.next = prev;
+            prev = Some(node);
         }
 
-        let mut l1p = &mut l1.as_mut().unwrap().next;
-        let mut l2p = &mut l2.as_mut().unwrap().next;
-        println!("l1: {:?}", l1p);
-        println!("l2: {:?}", l2p);
-
-        while let Some(n_l2) = l2p {
-            if let Some(n_l1) = l1p {
-                println!("sum: {:?}", sum);
-                println!("val 1: {:?}", n_l1.val);
-                println!("val 2: {:?}", n_l2.val);
-                sum = sum.max(n_l1.val + n_l2.val);
-                // println!("nl1: {:?}", n_l1);
-                // println!("nl2: {:?}", n_l2);
+        let mut p1 = &head;
+        let mut p2 = &prev;
+        while let Some(n2) = p2 {
+            if let Some(n1) = p1 {
+                sum = sum.max(n1.val + n2.val);
+                p1 = &n1.next;
+                p2 = &n2.next;
             }
-            l1p = &mut l1p.as_mut().unwrap().next;
-            l2p = &mut l2p.as_mut().unwrap().next;
         }
+
         sum
     }
 }
@@ -90,6 +80,20 @@ mod tests {
         let list = vec_to_list(&vec![1,2,3,4,6,8]);
         let result = Solution::pair_sum(list);
         assert_eq!(result, 9);
+    }
+
+    #[test]
+    fn t3() {
+        let list = vec_to_list(&vec![4,2,2,3]);
+        let result = Solution::pair_sum(list);
+        assert_eq!(result, 7);
+    }
+
+    #[test]
+    fn t4() {
+        let list = vec_to_list(&vec![1,100000]);
+        let result = Solution::pair_sum(list);
+        assert_eq!(result, 100001);
     }
 
 }
