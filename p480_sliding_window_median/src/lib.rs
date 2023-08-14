a2df34dbc5db29f6dff0b2181eeb8d358d8f3e49
Gustavo Basso <gubasso@eambar.net>
2023-08-14 19:04:20 -0300

    (locked) Issue: p480_sliding_window_median (#1)

--- p480_sliding_window_median/src/lib.rs
+++ p480_sliding_window_median/src/lib.rs
@@ -1,84 +1,123 @@
 struct Solution;
-use std::{collections::{BinaryHeap, HashMap}, cmp::Reverse};
+use std::{collections::{BinaryHeap, HashMap, BTreeMap}, cmp::Reverse};
 
-struct Median {
-    map: HashMap<i32, i32>,
-    max_heap: BinaryHeap<i32>,
-    min_heap: BinaryHeap<Reverse<i32>>,
+#[derive(Default, Debug)]
+struct MultiSet<T> {
+    count: usize,
+    data: BTreeMap<T, usize>,
 }
 
-impl Median {
+impl<T: Ord + Copy> MultiSet<T> {
 
-    fn new() -> Self {
-        Median {
-            map: HashMap::new(),
-            max_heap: BinaryHeap::new(),
-            min_heap: BinaryHeap::new(),
-        }
+    fn insert(&mut self, val: T) {
+        self.data.entry(val)
+            .and_modify(|c| *c+=1)
+            .or_insert(1);
+        self.count += 1;
     }
 
-    fn get_median(&self) -> f64 {
-        let (nx, ni) = (self.max_heap.len(), self.min_heap.len());
-        match (self.max_heap.peek(), self.min_heap.peek()) {
-            (Some(&max), Some(&min)) if nx == ni => (max as f64 + min.0 as f64) / 2.0,
-            (Some(&max), _) => max as f64,
-            _ => 0.0,
-        }
-    }
-
-    fn add_num(&mut self, num: i32) {
-        self.map.entry(num).and_modify(|n|*n+=1).or_insert(1);
-        self.max_heap.push(num);
-        self.min_heap.push(Reverse(self.max_heap.pop().unwrap()));
-        if self.min_heap.len() > self.max_heap.len() {
-            self.max_heap.push(self.min_heap.pop().unwrap().0);
-        }
-    }
-
-    fn pop(&mut self) -> Option<i32> {
-        let (nx, ni) = (self.max_heap.len(), self.min_heap.len());
-        if (nx, ni) == (0,0) {
-            return None;
-        }
-        if nx == ni {
-            return Some(self.min_heap.pop().unwrap().0);
-        }
-        if nx > ni {
-            return self.max_heap.pop();
+    fn remove(&mut self, val: &T) -> Option<T> {
+        if let Some(count) = self.data.get_mut(val) {
+            *count -= 1;
+            if *count == 0 {
+                self.data.remove(val);
+            }
+            self.count -= 1;
+            return Some(*val);
         }
         None
     }
 
-    fn remove_num(&mut self, num: i32) -> Option<i32> {
-        if !self.map.contains_key(&num) || self.map.get(&num).unwrap() == &0 {
-            return None;
+    fn last(&mut self) -> Option<T> {
+        self.data.iter().next_back().map(|(k, _)| *k)
+        // self.data.last_key_value().map(|(k,_)| *k)
+    }
+
+    fn pop_last(&mut self) -> Option<T> {
+        self.last().and_then(|l| self.remove(&l))
+    }
+
+    fn len(&self) -> usize {
+        self.count
+    }
+
+}
+
+#[derive(Default, Debug)]
+struct MedianFinder {
+    lo: MultiSet<i64>,
+    hi: MultiSet<Reverse<i64>>,
+}
+
+impl MedianFinder {
+
+    fn get_median(&mut self) -> Option<f64> {
+        let max = self.lo.last()?;
+        if self.lo.len() != self.hi.len() {
+            return Some(max as f64);
         }
+        let min = self.hi.last().unwrap().0;
+        Some((max + min) as f64 / 2.0)
+    }
 
-        let mut bank_to_return: Vec<i32> = vec![];
-        let mut removed_num: Option<i32> = None;
+    fn push(&mut self, val: i64) {
+        self.lo.insert(val);
+        self.hi.insert(Reverse(self.lo.pop_last().unwrap()));
+        // println!("hi: {:?}", self.hi);
+        if self.lo.len() < self.hi.len() {
+            self.lo.insert(self.hi.pop_last().unwrap().0);
+        }
+    }
 
-        while let Some(removed) = self.pop() {
-            self.map.entry(removed).and_modify(|n|*n+=1);
-            if removed == num {
-                break;
+    fn pop(&mut self, val: i64) -> Option<i64> {
+        if let Some(popped) = self.lo.remove(&val) {
+            if self.lo.len() < self.hi.len() {
+                self.lo.insert(self.hi.pop_last().unwrap().0);
             }
-            bank_to_return.push(removed);
+            return Some(popped);
         }
+        if let Some(popped) = self.hi.remove(&Reverse(val)) {
+            if self.lo.len() - self.hi.len() > 1  {
+                self.hi.insert(Reverse(self.lo.pop_last().unwrap()));
+            }
+            return Some(popped.0);
+        }
+        None
+    }
 
-        removed_num
+    fn balance(&mut self) {
+        while self.lo.len() < self.hi.len() {
+            self.lo.insert(self.hi.pop_last().unwrap().0);
+        }
+        while self.lo.len() - self.hi.len() > 1  {
+            self.hi.insert(Reverse(self.lo.pop_last().unwrap()));
+        }
     }
 
 }
 
 impl Solution {
     pub fn median_sliding_window(nums: Vec<i32>, k: i32) -> Vec<f64> {
-        let (mut i, mut j, n) = (0, k as usize, nums.len());
+        // println!("\n");
+        let (n, k) = (nums.len(), k as usize);
+        let nums = nums.into_iter().map(|e| e as i64).collect::<Vec<i64>>();
         let mut ans = Vec::with_capacity(n);
+        let mut mf = MedianFinder::default();
 
-        while j <= n {
-            // let heap = Median::from_slice(&nums[i..j]);
-            // ans.push(heap.get_median());
-            // i += 1; j += 1;
+        // for l in 0..k {
+        for n in nums.iter().take(k) {
+            mf.push(*n);
+        }
+        ans.push(mf.get_median().unwrap());
+
+        for i in k..nums.len() {
+            // println!("to add: {:?}, to remove: {:?}", nums[i], nums[i-k]);
+            mf.push(nums[i]);
+            mf.pop(nums[i-k]);
+            // println!("lo: {:?}", mf.lo);
+            // println!("hi: {:?}", mf.hi);
+            // println!("- median: {:?}", mf.get_median().unwrap());
+            ans.push(mf.get_median().unwrap());
         }
 
         ans
@@ -88,6 +127,23 @@ impl Solution {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // #[test]
+    // fn utils() {
+    //     println!("\n");
+    //     let nums = vec![1,3,-1,-3,5,3,6,7];
+    //     let mut median = Median::new();
+    //     for n in nums {
+    //         median.add_num(n);
+    //     }
+    //     println!("{:?}", median);
+    //     median.remove_num(-1);
+    //     println!("{:?}", median);
+    //     median.remove_num(3);
+    //     println!("{:?}", median);
+    //     median.remove_num(3);
+    //     println!("{:?}", median);
+    // }
 
     #[test]
     fn t1() {
@@ -101,6 +157,20 @@ mod tests {
         let nums = vec![1,2,3,4,2,3,1,4,2];
         let result = Solution::median_sliding_window(nums, 3);
         assert_eq!(result, vec![2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]);
+    }
+
+    #[test]
+    fn t3() {
+        let nums = vec![7,9,3,8,0,2,4,8,3,9];
+        let result = Solution::median_sliding_window(nums, 1);
+        assert_eq!(result, vec![7.00000,9.00000,3.00000,8.00000,0.00000,2.00000,4.00000,8.00000,3.00000,9.00000]);
+    }
+
+    #[test]
+    fn t4() {
+        let nums = vec![6,5,9,5,4,9,1,7,5,5];
+        let result = Solution::median_sliding_window(nums, 4);
+        assert_eq!(result, vec![5.50000,5.00000,7.00000,4.50000,5.50000,6.00000,5.00000]);
     }
 
 }
