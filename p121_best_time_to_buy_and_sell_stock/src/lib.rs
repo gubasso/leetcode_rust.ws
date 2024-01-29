31212acdaecd518996e0da556c48231700869d7e
Gustavo Basso <gubasso@eambar.net>
2024-01-29 20:39:03 -0300

    (up) Issue #p121_best_time_to_buy_and_sell_stock.

--- p121_best_time_to_buy_and_sell_stock/src/lib.rs
+++ p121_best_time_to_buy_and_sell_stock/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    struct Solution;
+
+    impl Solution {
+        pub fn max_profit(prices: Vec<i32>) -> i32 {
+            let mut l = 0;
+            let mut max_profit = 0;
+            for i in 0..prices.len() {
+                if prices[l] > prices[i] {
+                    l = i;
+                    continue;
+                }
+                max_profit = i32::max(max_profit, prices[i] - prices[l]);
+            }
+            max_profit
+        }
+    }
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn t1() {
+        let prices = vec![7,1,5,3,6,4];
+        let output = 5;
+        let result = Solution::max_profit(prices);
+        assert_eq!(result, output);
     }
+
+    #[test]
+    fn t2() {
+        let prices = vec![7,6,4,3,1];
+        let output = 0;
+        let result = Solution::max_profit(prices);
+        assert_eq!(result, output);
+    }
+
 }
